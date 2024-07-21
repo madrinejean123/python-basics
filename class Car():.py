b0b9d33480f5592abe927abd8fa3f836bb0c4544
Namulinde jean madrine <madrine@kali.jeanneNetwork.com>
2024-07-21 18:42:02 +0300

    classes and objects

---
+++ class Car():.py
@@ -0,0 +1,18 @@
+class Car():
+    def __init__(self,c,brand,) -> None:
+        self.color=c
+        self.name=brand
+        def move(self):
+            print("that car can move")
+# # object
+# me = Car("armygreen","toyota")
+# print(me.color)
+# print(me.name)
+
+# new = Car("black","bmw")
+# print(me.color)
+# print(me.name)
+
+next = Car("grey","markx")
+print(next.color)
+print(next.name)
---
+++ class and objects.py
@@ -0,0 +1,10 @@
+class Person():
+    _name = "jean"
+    _gender = "female"
+    def person(self):
+        print("she is studying now")
+# object
+girl = Person()
+print(girl._name)
+print(girl._gender)
+print(girl.person())
